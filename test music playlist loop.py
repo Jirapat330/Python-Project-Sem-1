b91a486c1933677a3680dca386c6806c128fa880
Jirapat330 <92516335+Jirapat330@users.noreply.github.com>
2021-11-05 15:06:49 +0700

    Add files via upload

---
+++ test music playlist loop.py
@@ -0,0 +1,18 @@
+playlist = []
+playlist.append("a")
+playlist.append("b")
+playlist.append("c")
+playlist.append("d")
+playlist.append("e")
+playlist.append("f")
+playlist.append("g")
+print(playlist)
+print(len(playlist),"\n")
+
+while True:
+    while True:
+        if len(playlist) > 0:
+            j = playlist.pop()
+            print(j)
+
+
